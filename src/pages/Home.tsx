d1e2649a91a07f6b9a2d14c636e507d8b2ec4848
WPC <1609253525@qq.com>
2022-05-04 22:53:18 +0800

    add menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import '../css/purecss/base.css'
 import '../css/purecss/grids.css';
+import '../css/purecss/menus.css'
 
 type HomeProps={
   links:String[];
@@ -8,10 +10,11 @@ type HomeProps={
 
 export function Home({links}:HomeProps) {
     return (
-        <div>
-            <nav className="pure-g">
-                {links.map((i)=>(<Link to={i as string} className="pure-u-1-6">{i}</Link>))}
-            </nav>
+            <div className="pure-menu pure-menu-horizontal">
+                <ul className="pure-menu-list">
+                        {links.map((i)=>(<li className="pure-menu-item">
+                            <Link to={i as string} className="pure-menu-link">{i}</Link></li>))}
+                </ul>
             <h1>Home</h1>
         </div>
     )
